beb24acc43b08275b5b82cf554213e4a258ddd90
Niklas Note <niklaslinz@hotmail.com>
2016-06-11 12:26:45 +0200

    Cleanup

--- src/main/java/de/nlinz/xeonSuite/portal/Listener/XeonPortal.java
+++ src/main/java/de/nlinz/xeonSuite/portal/Listener/XeonPortal.java
@@ -1,14 +1,7 @@
 package de.nlinz.xeonSuite.portal.Listener;
 
-import java.io.DataInputStream;
-import java.io.IOException;
-
-import org.bukkit.Bukkit;
-
-import de.nlinz.javaSocket.client.api.XeonSocketClientManager;
 import de.nlinz.javaSocket.client.events.SocketDataEvent;
 import de.nlinz.javaSocket.client.interfaces.IDataListener;
-import de.nlinz.xeonSuite.bukkit.XeonSuiteBukkit;
 
 public class XeonPortal implements IDataListener {
 
@@ -22,26 +15,7 @@ public class XeonPortal implements IDataListener {
 
 	@Override
 	public void onDataRecieve(SocketDataEvent event) {
-		// TODO Auto-generated method stub
-		DataInputStream in = XeonSocketClientManager.readDataInput(event.getStreamBytes());
-		String task = null;
-		String servername = null;
-		try {
-			servername = in.readUTF();
-
-			if (!servername.equalsIgnoreCase(XeonSuiteBukkit.getServerName())) {
-				return;
-			}
-
-			task = in.readUTF();
-			Bukkit.getLogger().info(task);
-			if (task.equalsIgnoreCase("some")) {
-
-			}
-
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		/* Nothing yet */
 	}
 
 }
